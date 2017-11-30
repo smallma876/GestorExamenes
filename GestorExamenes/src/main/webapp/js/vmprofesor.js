8e342c193330bad0e7b26887cea0ffcc0221aa0f
sergio.mallma876@gmail.com <smallmac@LIM-5CG5114LDC.usersad.everis.int>
2017-11-30 17:01:53 -0500

    hola

--- GestorExamenes/src/main/webapp/js/vmprofesor.js
+++
@@ -1,8 +0,0 @@
-var viewmodelprofesor = function(){
-	this.title = "Mantenimiento Profes";
-}
-var viewmodelprofesor2 = function(){
-	this.title = "mant";
-}
-ko.applyBindings(new viewmodelprofesor(),document.getElementById("profesorTitle"));
-ko.applyBindings(new viewmodelprofesor2(),document.getElementById("profesoresBody"));
